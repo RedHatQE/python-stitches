bef779b06d0821d83d402151f533a2d9b84dfc9b
dparalen <vetrisko@gmail.com>
2014-09-12 14:24:20 +0200

    Automatic commit of package [python-stitches] release [0.8-1].

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(name='stitches',
-    version='0.7',
+    version='0.8',
     description='Multihost actions toolbox',
     author='Vitaly Kuznetsov',
     author_email='vitty@redhat.com',
