531c443052bbf861c27312c118dc34888fd0652c
Radek Bíba <radek@redhat.com>
2020-03-05 12:33:13 +0100

    bump version to 0.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(name='stitches',
-    version='0.13',
+    version='0.14',
     description='Multihost actions toolbox',
     author='Vitaly Kuznetsov',
     author_email='vitty@redhat.com',
